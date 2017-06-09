d02b22f05b895c881b08a406bbbe9b74d3c3f84f
kiannguyen <kianbomba@gmail.com>
2017-06-09 20:15:41 +1200

    Invoker JS

--- Invoker.js
+++ Invoker.js
@@ -1,13 +1,10 @@
-/**
- * Created by kiannguyen on 9/06/17.
- */
-(function(Invoker){
-    "use strict";
-    var pattern = /\w[a-zA-z0-9._]\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}/;
-    var items = ["*", "[", "]", "(", ")", "^", "%", "#", "!", "-", "=", "+", "&", "$", "`", "~", " ", "{", "}", ",", "<", ">", "?", "/", "|"];
-    Invoker.isEmail =  function (str) {
+"use strict";
+var Invoker = {
+    isEmail: function (str) {
         var tmp = true;
+        var pattern = /\w[a-zA-z0-9._]\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}/;
         tmp = pattern.test(str);
+        var items = ["*", "[", "]", "(", ")", "^", "%", "#", "!", "-", "=", "+", "&", "$", "`", "~", " ", "{", "}", ",", "<", ">", "?", "/", "|"];
         for (var i = 0; i < items.length; i++) {
             if (str.indexOf(items[i]) > -1) {
                 tmp = false
@@ -18,19 +15,19 @@
             tmp = false;
         }
         return tmp;
-    };
+    },
 
-    Invoker.removeSpaces =  function (string_text) {
+    removeSpaces: function (string_text) {
         return string_text.replace(/^\s+|\s+$/g, '');
-    };
+    },
 
-    Invoker.replaceToCode =  function (Str) {
+    replaceToCode: function (Str) {
         var Str1 = Str.replace(/'/g, "&#39;"); // HTML CODE INSTEAD OF '
         Str1 = Str1.replace(/"/g, "&#34;");
         return Str1;
-    };
+    },
 
-    Invoker.checkSpecialCharacters =  function (string_text) {
+    checkSpecialCharacters: function (string_text) {
         var spCh = ["~", "`", "!", "@", "#", "$", ",", "%", "^", "&", "*", "(", ")", "=", "/", "+", "-", "<", ">", "?", "|", "';"];
         var tmp = false;
 
@@ -40,17 +37,15 @@
             }
         }
         return tmp;
-    };
+    },
 
-    Invoker.replaceToSpecialChar = function (str) {
+    replaceToSpecialChar: function (str) {
         var str1 = str.replace(/&#39;/, "'");
         str1 = str1.replace(/&#34;/, "\"")
         return str1;
-    };
+    },
 
-    Invoker.hello =  function () {
-        return "Hello Wrold";
-    };
-
-}(Invoker));
-
+    hello:  function(){
+        return "Hello World";
+    }
+};
